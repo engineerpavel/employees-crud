57918f0ba53e3687529a6337d56601eb86d05f82
Pavel Pogorelov <engineer.pavel@yandex.com>
2022-06-06 18:55:45 +0300

    Аватары

--- src/app/components/employee-table/employee-table.component.ts
+++ src/app/components/employee-table/employee-table.component.ts
@@ -12,4 +12,8 @@ export class EmployeeTableComponent {
 
   @Input()
   employees: Observable<EmployeeModel[]>;
+
+  getImagePath(email: string): string {
+    return `./assets/images/userpics/${email}.svg`;
+  }
 }
